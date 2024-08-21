9e5456d043d2646e069ab545e0923c2bf73773ca
Adachukwu Nnakenyi <nnakenyiadachukwu@yahoo.com>
2024-08-21 21:04:14 +0100

    created IOURepository

---
+++ restapiexercise/src/main/java/com/cbfacademy/restapiexercise/ious/IOURepository.java
@@ -0,0 +1,10 @@
+package com.cbfacademy.restapiexercise.ious;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface IOURepository extends ListCrudRepository<IOU, UUID> {
+
+}
